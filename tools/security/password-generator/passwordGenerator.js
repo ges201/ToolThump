6b4c80121fe25704ac2c8e29ba526947e91e3177
ges201 <gesmundoguglielmo@gmail.com>
2025-09-03 18:51:34 +0200

    Use secure random number generation for shuffling array in password generator

--- tools/security/password-generator/passwordGenerator.js
+++ tools/security/password-generator/passwordGenerator.js
@@ -248,7 +248,7 @@ const pg = {
 
     shuffleArray: function (array) {
         for (let i = array.length - 1; i > 0; i--) {
-            const j = Math.floor(Math.random() * (i + 1));
+            const j = this.getSecureRandom(i + 1);
             [array[i], array[j]] = [array[j], array[i]];
         }
         return array;
